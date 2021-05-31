582551d7172eded746e4c5b344d8a8dd6917bab5
Rohitn697 <53824509+Rohitn697@users.noreply.github.com>
2021-05-31 14:22:43 +0530

    added fragments

--- app/src/main/java/com/example/grocers/HomeActivity.java
+++ app/src/main/java/com/example/grocers/HomeActivity.java
@@ -2,52 +2,64 @@ package com.example.grocers;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.example.grocers.Fragments.GoOutFragment;
+import com.example.grocers.Fragments.GoldFragment;
+import com.example.grocers.Fragments.OrdersFragment;
+import com.example.grocers.Fragments.VideosFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
 
 public class HomeActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
-
-
         bottomNavigationView = findViewById(R.id.bottomNavBar);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        //default fragment for home activity
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new OrdersFragment()).commit();
     }
     private BottomNavigationView.OnNavigationItemSelectedListener navigation =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
                     switch (item.getItemId()){
                         case R.id.orders:
-                            Toast.makeText(HomeActivity.this, "orders", Toast.LENGTH_SHORT).show();
+                            selectedFragment = new OrdersFragment();
                             break;
 
                         case R.id.goldMember:
-                            Toast.makeText(HomeActivity.this, "goldMemberTest", Toast.LENGTH_SHORT).show();
+                            selectedFragment = new GoldFragment();
                             break;
 
                         case R.id.goOut:
-                            Toast.makeText(HomeActivity.this, "goOutTest", Toast.LENGTH_SHORT).show();
+                            selectedFragment = new GoOutFragment();
                             break;
 
                         case R.id.video:
-                            Toast.makeText(HomeActivity.this, "videoTest", Toast.LENGTH_SHORT).show();
+                            selectedFragment = new VideosFragment();
                             break;
                     }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,selectedFragment).commit();
                     return true;
                 }
             };
